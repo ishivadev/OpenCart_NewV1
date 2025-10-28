3319da19e0a2d607da5c68a571d090ac7f551d5d
shiva <shiva@host.docker.internal>
2025-10-28 21:11:57 +0530

    Docker addition with project

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -3,6 +3,7 @@ package testBase;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
@@ -10,11 +11,14 @@ import java.util.Properties;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
@@ -26,25 +30,65 @@ public class BaseClass {
 	public static WebDriver driver;
 	public Logger logger;
 	public Properties p;
+	String hub_url = "http://localhost:4444/wd/hub"; // Hub URL
 	
 	@BeforeClass(groups= {"Sanity","Regression","Master"})
-	@Parameters({"os","browser"})
-	public void setupApp(String os, String br) throws IOException
+	@Parameters({"os","browser"})  // <os, browser> received from 'xml' file 
+	public void setupApp(String os, String br) throws IOException	// passes the <os, browser> for further use
 	{
 		FileReader file = new FileReader(".//src//test//resources//config.properties");
 		p = new Properties();
 		p.load(file);
 		
-		
 		logger = LogManager.getLogger(this.getClass()); // Will get the class as per the call
 		
-		switch(br.toLowerCase())
-		{
-		case "chrome" : driver = new ChromeDriver(); break;
-		case "edge" : driver = new EdgeDriver(); break;
-		case "firefox" : driver = new FirefoxDriver(); break;
-		default : System.out.println("Invalid browser name.."); return;
 		
+		// When the execution environment is 'remote'
+		if(p.getProperty("execution_env").equalsIgnoreCase("remote"))
+		{
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			
+			if(os.equalsIgnoreCase("Windows"))
+			{
+				capabilities.setPlatform(Platform.WIN11);
+			}
+			else if(os.equalsIgnoreCase("Linux"))
+			{
+				capabilities.setPlatform(Platform.LINUX);
+			}
+			else if(os.equalsIgnoreCase("Mac"))
+			{
+				capabilities.setPlatform(Platform.MAC);
+			}
+			else 
+			{
+				System.out.println("OS does not matched."); 
+				return; 
+			}
+			
+			switch(br.toLowerCase())
+			{
+				case "chrome" : capabilities.setBrowserName("chrome"); break;
+				case "edge" :  capabilities.setBrowserName("MicrosoftEdge"); break;
+				case "firefox" : capabilities.setBrowserName("firefox"); break;
+				default : System.out.println("Browser does not matched."); return;
+			}
+		
+			driver = new RemoteWebDriver(new URL (hub_url), capabilities); // Setting up the 'driver'
+			
+		}
+		
+		
+		// When the execution environment is 'local'
+		if(p.getProperty("execution_env").equalsIgnoreCase("local"))
+		{
+			switch(br.toLowerCase())
+			{
+				case "chrome" : driver = new ChromeDriver(); break;
+				case "edge" : driver = new EdgeDriver(); break;
+				case "firefox" : driver = new FirefoxDriver(); break;
+				default : System.out.println("Invalid browser name.."); return;
+			}
 		}
 		
 		
@@ -55,6 +99,7 @@ public class BaseClass {
 		driver.get(p.getProperty("appURL"));
 		driver.manage().window().maximize();
 	}
+	
 	
 	@AfterClass(groups= {"Sanity","Regression","Master"})
 	public void tearDown() throws InterruptedException
